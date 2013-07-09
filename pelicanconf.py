b46b9b8e4737aaa0d412a72033a53aa856a36a28
Mitchell Stanton-Cook <mscook@gmail.com>
2013-07-09 10:55:27 +1000

    Config tweaks/themes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
 
 AUTHOR = u'Mitchell Stanton-Cook'
-SITENAME = u'UQ Microbial Genomics Laboratory of Scott Beatson'
+SITENAME = u'Beatson Lab'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Australia/Brisbane'
 
 DEFAULT_LANG = u'en'
 
@@ -16,16 +17,25 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('GitHub', 'https://github.com/organizations/BeatsonLab-MicrobialGenomics'),
+        ('SCMB@UQ', 'http://staff.scmb.uq.edu.au/staff/scott-beatson'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+
+THEME = os.path.expanduser("~/3rd-repos/pelican-themes/chunk")
+SITESUBTITLE = 'Microbial Genomics Laboratory'
+FOOTER_TEXT = '(c) 2013 Beatson Microbial Genomics Laboratory'
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+
+GITHUB_URL = 'http://github.com/BeatsonLab-MicrobialGenomics'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
