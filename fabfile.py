f5a5d3650e3ab24ee93d4d5629b9de03e58fe0b0
Mitchell Stanton-Cook <mscook@gmail.com>
2013-07-09 10:55:04 +1000

    Helper publish scripts

---
+++ fabfile.py
@@ -0,0 +1,45 @@
+#fabfile to generate and publish a Pelican blog as a user page to GitHub
+#Mitchell Stanton-Cook 2013 
+#m.stantoncook@gmail.com
+
+import os
+from   fabric.api import task
+
+
+def get_fab_script_location():
+    """
+    Return directory containing fabfile which corresponds to Pelican local base
+    """
+    return os.path.dirname(os.path.realpath(__file__))
+
+def parse_config():
+    """
+    Very simple parser to get the remote repo info
+    """
+    with open(os.path.join(get_fab_script_location(), "config.dat")) as fin:
+        lines = fin.readlines()
+    return lines[0].split('=')[-1].strip()
+    
+
+@task
+def generate():
+    """
+    Execute the Pelican Makefile
+    """
+    loc = get_fab_script_location()
+    os.chdir(loc)
+    os.system("make html")
+    return loc
+
+@task
+def publish():
+    """
+    Publish the site/blog to GitHub
+    """
+    loc = generate()
+    uname = parse_config()
+    os.chdir(loc)
+    os.system("ghp-import output")
+    os.system("git push git@github.com:username/username.github.io.git gh-pages:master".replace("username", uname))
+    return loc
+
