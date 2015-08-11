df5542bc3801cdf1bcc6493f6589029a78fc74eb
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-08-11 13:40:31 +1000

    Fix up some config

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,12 +34,20 @@ CWD = os.getcwd()
 THEME = os.path.join(CWD, "themes/pelican-bootstrap3")
 FAVICON = 'images/favicon.png'
 SITESUBTITLE = 'Microbial Genomics Laboratory'
-FOOTER_TEXT = '(c) 2013-2014 Beatson Microbial Genomics Laboratory'
+CUSTOM_LICENSE=' 2013-2015 Beatson Microbial Genomics Laboratory '
 
 
 #Tweak visually
 DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = True
+#About.rst  Data.rst  People  People.rst  Projects.rst  Publications.rst  Software.rst
+MENUITEMS = [('About','http://beatsonlab.com'),
+             ('Blog', 'http://beatsonlab.com/archives'),
+             ('Data','http://beatsonlab.com/pages/data.html'),
+             ('People','http://beatsonlab.com/pages/people.html'),
+             ('Projects', 'http://beatsonlab.com/pages/projects.html'),
+             ('Publications','http://beatsonlab.com/pages/publications.html'),
+             ('Software','http://beatsonlab.com/pages/software.html')]
+DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_SIDEBAR = False
 DISPLAY_TAGS_ON_SIDEBAR = False
 #MENUITEMS = 'Publications'
@@ -57,3 +65,6 @@ GOOGLE_ANALYTICS='UA-49094461-1'
 #PLUGIN_PATH = 'pelican-plugins/'
 #PLUGINS = ['better_figures_and_images']
 #RESPONSIVE_IMAGES = True
+
+#New 11/08/2015
+BOOTSTRAP_FLUID = True
